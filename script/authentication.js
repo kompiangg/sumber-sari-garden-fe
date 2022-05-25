2d92e6275979b3b43e6c6b9761b5d3034c8f08bc
Kompiang Gede Sukadharma <kompiang.sukadharma@gmail.com>
2022-05-25 09:04:16 +0800

    [#1] Fix navbar, create grid system, link to auth page

--- script/authentication.js
+++ script/authentication.js
@@ -2,6 +2,12 @@ const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
+const params = new URLSearchParams(window.location.search)
+
+if (params.get("register") == 1) {
+	container.classList.toggle("right-panel-active")
+}
+
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
 });
