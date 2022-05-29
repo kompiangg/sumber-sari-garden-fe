15a4048ec38c09e04eb53a5e2349f6f893cf5ef0
TrisnaWahyudi <wayantrisna79@gmail.com>
2022-05-29 19:08:52 +0800

    add order card and footer on order page

---
+++ script/order.js
@@ -0,0 +1,21 @@
+function increaseCount(a, b) {
+    var input = b.previousElementSibling;
+    var value = parseInt(input.value, 10); 
+    
+    value = isNaN(value)? 0 : value;
+    value ++;
+    input.value = value;
+
+    getTotal(value, b);
+  }
+  
+function decreaseCount(a, b) {
+    var input = b.nextElementSibling;
+    var value = parseInt(input.value, 10); 
+    
+    if (value > 1) {
+        value = isNaN(value)? 0 : value;
+        value --;
+        input.value = value;
+    }
+}
