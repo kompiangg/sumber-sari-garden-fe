77c5503f9ecc1b7a295d575ff55eea521e61effa
Kompiang Gede Sukadharma <kompiang.sukadharma@gmail.com>
2022-06-01 18:23:12 +0800

    [#42] Finished consume api on new product at landing page

--- script/index.js
+++ script/index.js
@@ -2,7 +2,8 @@ import config from "./util/config.js";
 import errorHandling from "./util/errorHandling.js"
 
 (async function(){
-  let coupon = await fetch(config.baseURL + '/inventory/coupons?limit=3')
+  const coupon = await fetch(config.baseURL + '/inventory/coupons?limit=3')
+                .then(response => errorHandling.HandlingFetchError(response))
                 .then(response => response.json())
                 .catch(error => errorHandling.PrintError(error))
   
@@ -46,4 +47,74 @@ import errorHandling from "./util/errorHandling.js"
   
   carouselIndicators.innerHTML = carouselButton
   innerCarousel.innerHTML = willInserted
+})();
+
+(async function(){
+  const itemFetch = await fetch(
+    config.baseURL + '/inventory/products', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        'order_type': 1,
+        'limit': 12
+      })
+    }).then(response => errorHandling.HandlingFetchError(response))
+      .then(response => response.json())
+      .catch(error => errorHandling.PrintError(error))
+
+  if (itemFetch.error != null) {
+    errorHandling.PrintError(itemFetch.error.message)
+    return
+  }
+
+  const gridMenu = document.querySelector('.grid-menu')
+
+  itemFetch.data.products.forEach((element, index) => {
+    if (index < 9) {
+      const willInsert = document.createElement('div')
+      willInsert.classList = 'col-3'
+      willInsert.innerHTML = `
+        <div class="product-image">
+            <img src="${element.picture_url}" class="img-fluid">
+        </div>
+        
+        <div class="product-info">
+            <h5 class="fs-5">${element.product_name}</h5>
+            
+            <div class="category-name">
+              <em>${element.category_name}</em>
+            </div>
+  
+            <h6 class="fs-7">IDR ${element.price}</h6>
+  
+            <svg id="i-cart" xmlns="http://www.w3.org/2000/svg" viewBox="-0 -7 32 45" width="30" height="30" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                <path d="M6 6 L30 6 27 19 9 19 M27 23 L10 23 5 2 2 2" />
+                <circle cx="25" cy="27" r="2" />
+                <circle cx="12" cy="27" r="2" />
+            </svg>
+        </div>
+      `
+      gridMenu.appendChild(willInsert)
+    }
+  })
+
+  const carouselInner = gridMenu.parentNode.parentNode.querySelector('.carousel-inner')
+  console.log(carouselInner);
+  itemFetch.data.products.forEach((element, index) => {
+    if (index >= 9) {
+      const willInsert = document.createElement('div')
+      if (index == 9) {
+        willInsert.classList = 'carousel-item active'
+      } else {
+        willInsert.classList = 'carousel-item'
+      }
+      willInsert.innerHTML = `
+        <img src="${element.picture_url}" class="d-block w-100" alt="${element.product_name}">
+      `
+
+      carouselInner.appendChild(carouselInner)
+    }
+  })
 })();
